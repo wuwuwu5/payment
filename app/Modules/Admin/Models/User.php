f5dd6aa39513891d3c299d90c50610912175d37e
wujian <1531251231321@163.com>
2020-06-10 14:18:27 +0800

    17

--- app/Modules/Admin/Models/User.php
+++ app/Modules/Admin/Models/User.php
@@ -64,7 +64,7 @@ class User extends Authenticatable
      */
     public function setPasswordAttribute($value)
     {
-        return bcrypt($value);
+        return $this->attributes['password'] = bcrypt($value);
     }
 
 
